834e8cbe3e4f226066d5939e59557e60fefbd197
Daffa Rabbanee <daffarabbanee@gmail.com>
2020-02-20 20:02:16 +0700

    my first commit

---
+++ Kotlin Fundamental/Latihan/Latihan/src/Task.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val valueA = 65
+    val valueB = 52
+    val valueC = 78
+
+    val resultA = calculate(valueA, valueB, valueC)
+    val resultB = calculate(valueA, valueB, null)
+
+    println("""
+        ResultA is $resultA
+        ResultB is $resultB
+    """.trimIndent())
+}
+
+fun calculate(valueA: Int, valueB: Int, valueC: Int?): Int {
+    // TODO 1
+    val result = 0
+    return generateResult(result)
+}
+
+// TODO 2
+fun generateResult(result: Int) : Int = 0
